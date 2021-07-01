7f0258a82fe6a54618d4523f245047ae746faf98
Neha Sahu <nehasahu9424@gmail.com>
2021-07-01 07:10:00 +0530

    Added entity

---
+++ src/main/java/com/example/employee/management/Entity/Employee.java
@@ -0,0 +1,98 @@
+package com.example.employee.management.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.transaction.Transactional;
+
+@Entity
+@Table(name="employee")
+@Transactional
+public class Employee {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="Id")
+	private long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	
+	
+	public Employee() {
+		
+	}
+	
+	//create Constructor
+	
+	//default Constructor
+	
+	
+	
+	//Parametrized constructor
+	public Employee(String firstName, String lastName, String email) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+  
+	//Parametrized constructor
+
+	public Employee(long id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+
+
+	//create Getters and Setters
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	//tostring method
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+	
+	
+
+}
