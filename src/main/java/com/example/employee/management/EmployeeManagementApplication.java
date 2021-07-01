81fa88d54b42c974b360d9ddc0233eb58f483d41
Neha Sahu <nehasahu9424@gmail.com>
2021-07-01 07:08:50 +0530

    Main app file added

---
+++ src/main/java/com/example/employee/management/EmployeeManagementApplication.java
@@ -0,0 +1,14 @@
+package com.example.employee.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+	}
+
+}
